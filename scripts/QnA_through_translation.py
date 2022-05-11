5fbfb4a9a097d3cae91145ee7edff8a76598762a
rantaplanb <csd4457@csd.uoc.gr>
2022-05-11 22:46:47 +0300

    Housekeeping.

--- scripts/QnA_through_translation.py
+++ scripts/QnA_through_translation.py
@@ -1,13 +1,19 @@
 import csv, json, requests
 from utils.translator import translate
 from transformers import pipeline
+import time
 
-print("Script starts...")
-output_file = '../resources/csv_files/questions_with_answers_from_all_models_with_bing_translator.csv'
+input_file = '../resources/json_files/squad_QnA_dataset.json'
+output_file = '../resources/csv_files/questions_with_answers_from_all_models.csv'
+
+translator = 'bing'
 
 def answer_question(context, question, model):
+    start = time.time()
     question_answerer = pipeline(task="question-answering", model = model)
-    return question_answerer(question = question, context = context)
+    answer = question_answerer(question = question, context = context)
+    end = time.time()
+    return answer, end-start
 
 def fetch_data():
     response = requests.get("http://users.ics.forth.gr/mountant/files/greek.json")
@@ -15,13 +21,13 @@ def fetch_data():
     return json.loads(json_qna_str)["data"]
 
 def get_dataset():
-    f = open('../resources/json_files/squad_QnA_dataset_cut.json')
+    f = open(input_file)
     return json.load(f)["data"]
 
 print("Getting data...")
 data = get_dataset()
 
-headers = ['question', 'model', 'score', 'start', 'end', 'original_model_answer', 'translated_model_answer', 'correct_answer']
+headers = ['question', 'model', 'model_response_time', 'score', 'start', 'end', 'original_model_answer', 'translated_model_answer', 'correct_answer']
 
 models = [
     "deepset/roberta-base-squad2",
@@ -35,27 +41,26 @@ models = [
     "deepset/bert-base-cased-squad2",
     "bert-large-cased-whole-word-masking-finetuned-squad"]
 
-# with open(output_file, 'a', encoding='UTF16') as file:
-#     writer = csv.writer(file)
-#     writer.writerow(headers)
+with open(output_file, 'a', encoding='UTF16') as file:
+    writer = csv.writer(file)
+    writer.writerow(headers)
 
 for subject in data:
     paragraphs = subject["paragraphs"]
     print('\n-------------------------\n')
-    for QnA in paragraphs: #For each context 
-        en_context = translate(QnA['context'], 'bing', 'el', 'en') 
+    for QnA in paragraphs:      
+        en_context = translate(QnA['context'], translator, 'el', 'en')
         print('Context:', en_context)
-        for qna in QnA["qas"]: #For each question
-            en_question = translate(qna["question"], 'bing', 'el', 'en')
+        for qna in QnA["qas"]:
+            en_question = translate(qna["question"], translator, 'el', 'en')
             print('Question: ', en_question)
             results = []
             question = en_question
-            for i in range(len(models)): #For each model
-                result = answer_question(en_context, en_question, models[i])
-                results.append([question, models[i], result['score'], result['start'], result['end'], result['answer'], translate(result['answer'], 'bing', src='en', dest='el'), qna['answers'][0]['text']])
+            for i in range(len(models)):
+                result, elapsed_time = answer_question(en_context, en_question, models[i])
+                results.append([question, models[i], elapsed_time, result['score'], result['start'], result['end'], result['answer'], translate(result['answer'], translator, src='en', dest='el'), qna['answers'][0]['text']])
                 question = ""
-
             with open(output_file, 'a', encoding='UTF16') as file:
                 writer = csv.writer(file)
-                for i in range(len(results)): #For each model answer
+                for i in range(len(results)):
                     writer.writerow(results[i])
