667bb82fc3a15d5d2e62491164b786542ffb4446
rantaplanb <csd4457@csd.uoc.gr>
2022-05-17 09:41:41 +0300

    Added more model evaluation statistics.

--- scripts/evaluation/evaluate_models_on_our_collection.py
+++ scripts/evaluation/evaluate_models_on_our_collection.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+from functools import cmp_to_key
 
 translator = 'bing'
 total_contexts = 20
@@ -53,24 +54,122 @@ def get_model_scores(model):
         scores.append(scores_col[current_index])
     return scores
 
-def print_basic_model_statistics(model_results):
-    pass
+def compare_model_results(res1, res2):
+    score1 = res1['yes'] + float(res1['partially']) / 2
+    score2 = res2['yes'] + float(res2['partially']) / 2
+    if score1 > score2:
+        return 1
+    elif score1 == score2:
+        return 0
+    else:
+        return -1
 
-def print_basic_question_statistics(question_results):
-    pass
+def print_model_statistics(model_results):
+    model_results.sort(key=cmp_to_key(compare_model_results))
+    
+    for result in model_results:
+        print('For model', result['model'], ':')
+        print('Correct:', result['yes'])
+        print('Partially correct:', result['partially'])
+        print('Wrong:', result['no'])
+        print('Percentage:', "{:.2f}".format((result['yes'] + float(result['partially'])/2) / (result['yes'] + result['partially'] + result['no'])*100), '%')
+        print('---------------------------------------------------')
+
+def compare_question_results(ques1, ques2):
+    score1 = ques1['yes'] + float(ques1['partially']) / 2
+    score2 = ques2['yes'] + float(ques2['partially']) / 2
+    if score1 > score2:
+        return 1
+    elif score1 == score2:
+        return 0
+    else:
+        return -1
+
+def print_yellow():
+    print('\033[01;33m', end='', sep='')
+
+def reset_print_color():
+    print('\033[01;37m', end='', sep='')
+
+def print_question_statistics(question_results):
+    question_results.sort(key=cmp_to_key(compare_question_results))
+
+    invalid_questions = []
+    fully_wrong_questions = []
+    exactly_one_correct = []
+    at_least_one_correct = []
+    correct_or_partially = []
+    fully_correct_questions = []
+    for result in question_results:
+        cur_question = result['question']
+        if result['invalid'] == 10:
+            invalid_questions.append(cur_question)
+        elif result['yes'] == 10:
+            fully_correct_questions.append(cur_question)
+        elif result['no'] == 10:
+            fully_wrong_questions.append(cur_question)
+        
+        if result['yes'] > 0:
+            at_least_one_correct.append(cur_question)
+        if result['yes'] == 1:
+            exactly_one_correct.append(cur_question)
+        if result['no'] == 0 and result['invalid'] == 0:
+            correct_or_partially.append(cur_question)
+
+    print('\033[01;31m','------------------------------------------------------------','\033[00m', sep='')
+    print_yellow()
+    print(len(invalid_questions), ' questions were invalid due to incorrect translation: ')
+    print('\033[01;31m','------------------------------------------------------------','\033[00m', sep='')
+    for q in invalid_questions:
+        print(q)
+    print('\033[01;31m','\n------------------------------------------------------------','\033[00m', sep='')
+    print_yellow()
+    print(len(fully_wrong_questions), 'questions were answered completely wrong: ')
+    print('\033[01;31m','------------------------------------------------------------','\033[00m', sep='')
+    for q in fully_wrong_questions:
+        print(q)
+    print('\033[01;31m','\n------------------------------------------------------------','\033[00m', sep='')
+    print_yellow()
+    print(len(exactly_one_correct), 'questions were answered correctly by exactly one model: ')
+    print('\033[01;31m','------------------------------------------------------------','\033[00m', sep='')
+    for q in exactly_one_correct:
+        print(q)
+    print('\033[01;31m','\n------------------------------------------------------------','\033[00m', sep='')
+    print_yellow()
+    print(len(at_least_one_correct), 'questions were answered correctly by at least one model: ')
+    print('\033[01;31m','------------------------------------------------------------','\033[00m', sep='')
+
+    for q in at_least_one_correct:
+        print(q)
+    print('\033[01;31m','\n------------------------------------------------------------','\033[00m', sep='')
+    print_yellow()
+    print(len(correct_or_partially), 'questions had no wrong answers (only correct or partially correct): ')
+    print('\033[01;31m','------------------------------------------------------------','\033[00m', sep='')
+
+    for q in correct_or_partially:
+        print(q)
+    print('\033[01;31m','\n------------------------------------------------------------','\033[00m', sep='')
+    print_yellow()
+    print(len(fully_correct_questions), 'questions were answered completely correct: ')
+    print('\033[01;31m','------------------------------------------------------------','\033[00m', sep='')
+
+    for q in fully_correct_questions:
+        print(q)
+    
 
 if __name__ == "__main__":
     model_results = []
     for i in range(model_count):
         model_results.append(get_model_results(models[i]))
+    invalid_count = model_results[-1]['invalid'] * 10
+    #print_model_statistics(invalid_count)
 
     question_results = []
     for i in range(total_contexts):
         for j in range(questions_per_context):
             question_results.append(get_question_results(i, j))
-
+    print_question_statistics(question_results)
     
-
     # converted_bing_model_evaluation = convert_model_is_correct_column_to_num(bing_model_evaluation)
 
     # r = np.corrcoef(np.array(converted_bing_model_evaluation), np.array(bing_prediction_scores))
