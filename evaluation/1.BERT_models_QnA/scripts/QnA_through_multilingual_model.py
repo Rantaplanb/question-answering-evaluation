ef2060a2b6ba7a82688035f078d70dbf72cc11a4
rantaplanb <csd4457@csd.uoc.gr>
2022-06-13 17:46:14 +0300

    Major refactoring vol2. Retouched step 1 of the evaluation process.

---
+++ evaluation/1.BERT_models_QnA/scripts/QnA_through_multilingual_model.py
@@ -0,0 +1,106 @@
+import csv, json, os
+from transformers import pipeline
+import time
+
+def answer_question(context, question, model):
+    start = time.time()
+    question_answerer = pipeline(task="question-answering", model = model)
+    answer = question_answerer(question = question, context = context)
+    end = time.time()
+    return answer, end-start
+
+
+def get_dataset(filename):
+    if filename == 'custom_QnA_dataset.json':
+        f = open('../../../resources/json_files/' + filename, encoding='UTF16')
+        return json.load(f)["collection"]
+    elif filename == 'squad_QnA_dataset.json':
+        f = open('../../../resources/json_files/' + filename, encoding='UTF8')
+        return json.load(f)["data"]
+
+
+def write_headers(filename, headers):
+    with open('../output_data/' + filename, 'w', encoding='UTF16') as file:
+        writer = csv.writer(file)
+        writer.writerow(headers)
+
+
+def QnA_on_custom_dataset(dataset, output_file, model):
+    headers = ['question', 'correct_answer', 'model_answer', 'model_response_time', 'confidence_score']
+    write_headers(output_file, headers)
+    for set in dataset:
+        context = set['context']
+        questions = set['questions']
+        answers = set['answers']
+
+        print('Context: ', context)
+        for i in range(len(questions)):
+            question = questions[i]
+            print('Question: ', question)
+            result, elapsed_time = answer_question(context, question, model)
+            result_row = [question, answers[i], result['answer'], elapsed_time, result['score']]
+            question = ""
+            with open('../output_data/' + output_file, 'a', encoding='UTF16') as file:
+                writer = csv.writer(file)
+                writer.writerow(result_row)
+
+
+def QnA_on_xquad(dataset, output_file, model):
+    headers = ['question', 'correct_answer', 'model_answer', 'model_response_time', 'confidence_score']
+    write_headers(output_file, headers)
+
+    for subject in dataset:
+        paragraphs = subject["paragraphs"]
+        for QnA in paragraphs:
+            context = QnA['context']
+            print('Context: ', context)
+            for qna in QnA["qas"]:
+                question = qna["question"]
+                print('Question: ', question)
+                result, elapsed_time = answer_question(context, question, model)
+                result_row = [question, qna['answers'][0]['text'], result['answer'], elapsed_time, result['score']]
+                question = ""
+                with open('../output_data/' + output_file, 'a', encoding='UTF16') as file:
+                    writer = csv.writer(file)
+                    writer.writerow(result_row)
+
+
+def select_input_file(input_dir_path):
+    input_files = os.listdir(input_dir_path)
+    print('The available input files are:')
+    for i in range(len(input_files)):
+        print(str(i + 1)  + ') ' + input_files[i])
+    choice = input('Select input file number: ')
+    if choice.isdigit() and int(choice) > 0 and int(choice) <= len(input_files):
+        return input_files[int(choice ) - 1]
+    else:
+        print('Invalid input, expected number in range of (0 - ' + str(len(input_files)) + ')')
+        print('Terminating ...')
+        exit(0)
+
+"""
+---Script Configuration---
+- To select a different multilingual QnA BERT model:
+  In function main, change the value of the model variable
+  to the name of your multilingual model of choice.
+- To add your own input QnA dataset:
+    1) Add your own json file to the ${PROJECT_ROOT}/resources/json_files/ directory.
+    2) Modify get_dataset() to load your json file.
+    3) Implement your own QnA_on_${DATASET_NAME} function using answer_question(). 
+    4) Add a case in main() for your json dataset and call your function implemented in (3).
+"""
+if __name__ == '__main__':
+    model = "deepset/xlm-roberta-large-squad2"
+    input_dir = '../../../resources/json_files/'
+    input_filename = select_input_file(input_dir)
+
+    dataset = get_dataset(input_filename)
+
+    if input_filename == 'custom_QnA_dataset.json':
+        output_filename = 'QnA_on_custom_dataset_with_' + model.replace('/', '-') + '.csv'
+        QnA_on_custom_dataset(dataset, output_filename, model)
+    elif input_filename == 'squad_QnA_dataset.json':
+        output_filename = 'QnA_on_xquad_with_' + model.replace('/', '-') + '.csv'
+        QnA_on_xquad(dataset, output_filename, model)
+    else:
+        print("No man's land.")
--- evaluation/1.BERT_models_QnA/scripts/QnA_through_translation_on_custom_dataset.py
+++ evaluation/1.BERT_models_QnA/scripts/QnA_through_translation_on_custom_dataset.py
@@ -1,12 +1,9 @@
-import csv, json, requests
-from utils.translator import translate
+from asyncore import write
+import csv, json, time, threading, os, sys, inspect
 from transformers import pipeline
-import time
-
-input_file = '../resources/json_files/greek_text_QnA_collection.json'
-output_file = '../resources/csv_files/questions_with_answers_from_all_models_on_our_collection_bing.csv'
-
-translator = 'bing'
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname( \
+    os.path.abspath(inspect.getfile(inspect.currentframe()))))))
+from utils.translator import translate
 
 def answer_question(context, question, model):
     start = time.time()
@@ -15,21 +12,46 @@ def answer_question(context, question, model):
     end = time.time()
     return answer, end-start
 
-def fetch_data():
-    response = requests.get("http://users.ics.forth.gr/mountant/files/greek.json")
-    json_qna_str = response.text
-    return json.loads(json_qna_str)["data"]
+def get_dataset(filename):
+    if filename == 'custom_QnA_dataset.json':
+        f = open('../../../resources/json_files/' + filename, encoding='utf-16')
+        return json.load(f)["collection"]
+    elif filename == 'squad_QnA_dataset.json':
+        f = open('../../../resources/json_files/' + filename, encoding='UTF8')
+        return json.load(f)["data"]
 
-def get_dataset():
-    f = open(input_file, encoding='utf-16')
-    return json.load(f)["collection"]
 
-print("Getting data...")
-sets = get_dataset()
+def write_headers(filename, headers):
+    with open('../output_data/' + filename, 'w', encoding='UTF16') as file:
+        writer = csv.writer(file)
+        writer.writerow(headers)
 
-headers = ['question', 'model', 'model_response_time', 'score', 'start', 'end', 'original_model_answer', 'translated_model_answer', 'correct_answer', 'translated_correct_answer']
 
-models = [
+def select_input_file(input_dir_path):
+    input_files = os.listdir(input_dir_path)
+    print('The available input files are:')
+    for i in range(len(input_files)):
+        print(str(i + 1)  + ') ' + input_files[i])
+    choice = input('Select input file number: ')
+    if choice.isdigit() and int(choice) > 0 and int(choice) <= len(input_files):
+        return input_files[int(choice ) - 1]
+    else:
+        print('Invalid input, expected number in range of (0 - ' + str(len(input_files)) + ')')
+        print('Terminating ...')
+        exit(0)
+
+def select_translator():
+    print('The available translators are:\n1) Bing\n2) Helsinki ')
+    choice = input('Select translator number: ')
+    if choice.isdigit() and int(choice) > 0 and int(choice) <= 2:
+        return 'bing' if choice == 1 else 'helsinki'
+    else:
+        print('Invalid input, expected number in range of (0 - 2)')
+        print('Terminating ...')
+        exit(0)
+
+def get_models():
+    return [
     "deepset/roberta-base-squad2",
     "bert-large-uncased-whole-word-masking-finetuned-squad",
     "distilbert-base-cased-distilled-squad",
@@ -39,31 +61,51 @@ models = [
     "deepset/minilm-uncased-squad2",
     "dmis-lab/biobert-large-cased-v1.1-squad",
     "deepset/bert-base-cased-squad2",
-    "bert-large-cased-whole-word-masking-finetuned-squad"]
+    "bert-large-cased-whole-word-masking-finetuned-squad"
+    ]
 
-with open(output_file, 'a', encoding='UTF16') as file:
-    writer = csv.writer(file)
-    writer.writerow(headers)
 
-for set in sets:
-    context = set['context']
-    questions = set['questions']
-    answers = set['answers']
+def QnA_on_custom_dataset(dataset, output_file):
+    headers = ['question', 'model', 'gr_correct_answer', 'gr_model_answer', 'en_correct_answer', 'en_model_answer', 'model_response_time', 'confidence_score']
+    write_headers(output_file, headers)
 
-    en_context = translate(context, translator, 'el', 'en')
-    print('Context:', en_context)
-    for i in range(len(questions)):
-        en_question = translate(questions[i], translator, 'el', 'en')
-        print('Question: ', en_question)
-        results = []
-        tmp_question = en_question
-        for j in range(len(models)):
-            result, elapsed_time = answer_question(en_context, en_question, models[j])
-            gr_model_answer = translate(result['answer'], translator, src='en', dest='el')
-            translated_correct_answer = translate(answers[i], translator, src='el', dest='en')
-            results.append([tmp_question, models[j], elapsed_time, result['score'], result['start'], result['end'], result['answer'], gr_model_answer, answers[i], translated_correct_answer])
-            tmp_question = ""
-        with open(output_file, 'a', encoding='UTF16') as file:
-            writer = csv.writer(file)
-            for k in range(len(results)):
-                writer.writerow(results[k])
+    for set in dataset:
+        context = set['context']
+        questions = set['questions']
+        answers = set['answers']
+        models = get_models()
+        en_context = translate(context, translator, 'el', 'en')
+        print('Context:', en_context)
+        for i in range(len(questions)):
+            en_question = translate(questions[i], translator, 'el', 'en')
+            print('Question: ', en_question)
+            result_rows = []
+            for j in range(len(models)):
+                result, elapsed_time = answer_question(en_context, en_question, models[j])
+                gr_model_answer = translate(result['answer'], translator, src='en', dest='el')
+                translated_correct_answer = translate(answers[i], translator, src='el', dest='en')
+                result_rows.append([questions[i], models[j], answers[i], gr_model_answer, translated_correct_answer, result['answer'], elapsed_time, result['score']])
+            with open(output_file, 'a', encoding='UTF16') as file:
+                writer = csv.writer(file)
+                for k in range(len(result_rows)):
+                    writer.writerow(result_rows[k])
+
+
+def QnA_on_xquad(dataset, output_file):
+    pass
+
+if __name__ == '__main__':
+    input_dir = '../../../resources/json_files/'
+    input_filename = select_input_file(input_dir)
+    translator = select_translator()
+
+    dataset = get_dataset(input_filename)
+
+    if input_filename == 'custom_QnA_dataset.json':
+        output_filename = 'QnA_on_custom_dataset_with_' + translator + '.csv'
+        QnA_on_custom_dataset(dataset, output_filename)
+    elif input_filename == 'squad_QnA_dataset.json':
+        output_filename = 'QnA_on_xquad_with_' + translator + '.csv'
+        QnA_on_xquad(dataset, output_filename)
+    else:
+        print("No man's land.")
