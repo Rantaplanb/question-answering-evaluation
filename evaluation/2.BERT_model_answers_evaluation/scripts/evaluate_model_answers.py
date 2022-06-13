2f96fc8262c589c5e32eb4b50d19fac8937791dc
rantaplanb <csd4457@csd.uoc.gr>
2022-06-13 22:17:57 +0300

    More refactoring on step 2 of the evaluation process.

--- evaluation/2.BERT_model_answers_evaluation/scripts/evaluate_model_answers.py
+++ evaluation/2.BERT_model_answers_evaluation/scripts/evaluate_model_answers.py
@@ -1,11 +1,68 @@
-import spacy, csv, re
+import spacy, csv, os
 import pandas as pd
 import Levenshtein
 from difflib import SequenceMatcher
 from sentence_transformers import SentenceTransformer, util
 
-input_file = '../../resources/csv_files/questions_with_answers_from_all_models_on_our_collection_bing.csv'
-output_file = '../../resources/csv_files/new_csv_files/qna_on_our_collection_bing_scores.csv'
+
+nlp = spacy.load("en_core_web_lg")
+sentence_transformer = SentenceTransformer('distilbert-base-nli-mean-tokens')
+sentence_transformer_multilingual = SentenceTransformer('sentence-transformers/distiluse-base-multilingual-cased')
+
+
+def select_input_file(input_dir_path):
+    input_files = os.listdir(input_dir_path)
+    print('The available input files are:')
+    for i in range(len(input_files)):
+        print(str(i + 1)  + ') ' + input_files[i])
+    choice = input('Select input file number: ')
+    if choice.isdigit() and int(choice) > 0 and int(choice) <= len(input_files):
+        return input_files[int(choice ) - 1]
+    else:
+        print('Invalid input, expected number in range of (0 - ' + str(len(input_files)) + ')')
+        print('Terminating ...')
+        exit(0)
+
+
+def get_QnA_results(filepath):
+    data = pd.read_csv(filepath, encoding='UTF-16')
+    return {
+        'questions' : list(data['question']),
+        'models' : list(data['model'][:list(data['model'][1:]).index(data['model'][0]) + 1]), # Get models from csv
+        'gr_correct_answers' : list(data['gr_correct_answer']),
+        'gr_model_answers' : list(data['gr_model_answer']),
+        'en_correct_answers' : list(data['en_correct_answer']),
+        'en_model_answers' : list(data['en_model_answer']),
+        'confidence_scores' : list(data['confidence_score'])
+    }
+
+
+def get_output_filename(input_filename):
+    if 'custom' in input_filename:
+        return 'evaluated_answers_on_custom_dataset_with_' + input_filename[input_filename.index('with_') + 5 : - 4] + '.csv'        
+    elif 'xquad' in input_filename:
+        return 'evaluated_answers_on_xquad_with_' + input_filename[input_filename.index('with_') + 5 : - 4] + '.csv'
+    else:
+        print("No man's land.")
+        exit(0)
+
+
+def format_answer(answer):
+    return answer.lower().replace('.', '').replace(',', '').replace('\'', '')\
+                 .replace('\"', '').replace('-', '').replace('%', '').replace(':', '')
+
+def calc_nlp_score(correct_answer, model_answer):
+    return float("{:.3f}".format(nlp(model_answer).similarity(nlp(correct_answer))))
+
+def calc_levenshtein_score(correct_answer, model_answer):
+    return float("{:.3f}".format(1 - (float(Levenshtein.distance(model_answer, correct_answer)) / (max(len(model_answer), len(correct_answer))))))
+
+def calc_substring_score(correct_answer, model_answer):
+    return float("{:.3f}".format(SequenceMatcher(None, model_answer, correct_answer).ratio()))
+
+def calc_sentence_transformer_score(correct_answer, model_answer):
+    sentence_embeddings = sentence_transformer.encode([model_answer, correct_answer])
+    return float("{:.3f}".format(util.pytorch_cos_sim(sentence_embeddings[0], sentence_embeddings[1])[0][0].item()))
 
 def count_common_words(s0, s1):
     s0 = s0.lower()
@@ -14,80 +71,82 @@ def count_common_words(s0, s1):
     s1List = s1.split(" ")
     return len(list(set(s0List)&set(s1List)))
 
-data = pd.read_csv(input_file, encoding='UTF-16')
-model_answers = data['english_model_answer']
-correct_answers = data['english_correct_answer']
-gr_model_answers = data['greek_model_answer']
-gr_correct_answers = data['greek_correct_answer']
-questions = data['question']
-models = data['model'][:10]
-model_prediction_scores = data['score']
-is_correct_col = data['is_correct']
+def calc_f1_score(correct_answer, model_answer):
+    common_words_count = count_common_words(model_answer, correct_answer)
+    precision = common_words_count / len(model_answer.split(" "))
+    recall = common_words_count / len(correct_answer.split(" "))
+    return 0 if precision == 0 and recall == 0 else (2 * precision * recall) / (precision + recall)
 
-nlp = spacy.load("en_core_web_lg")
-sentence_transformer = SentenceTransformer('distilbert-base-nli-mean-tokens')
-sentence_transformer_multilingual = SentenceTransformer('sentence-transformers/distiluse-base-multilingual-cased')
 
-headers = ['question', 'model', 'en_model_answer', 'en_correct_answer', 'modified_model_answer', 'modified_correct_answer',  'gr_model_answer', 'gr_correct_answer', 'model_prediction_score', 'nlp_score', 'levenshtein_score', 'substring_score', 'sentence_transformers_score', 'greek_sentence_transformers_score', 'F1_score', 'weighted_total_score', 'is_correct']
+def calculate_scores(correct_answers, model_answers, weights):
+    scores = {'nlp': [], 'levenshtein': [], 'substring': [], 'sentence_transformers': [], 'f1': []}
+    total_scores = []
+    score_calculators = {
+        'nlp': calc_nlp_score, 
+        'levenshtein': calc_levenshtein_score, 
+        'substring': calc_substring_score, 
+        'sentence_transformers': calc_sentence_transformer_score, 
+        'f1': calc_f1_score
+        }
 
-greek_to_english = {}
+    for i in range(len(correct_answers)):
+        total_score = 0
+        correct_answer = format_answer(correct_answers[i])
+        model_answer = format_answer(model_answers[i])
 
-with open(output_file, 'a', encoding='UTF16') as file:
-    writer = csv.writer(file)
-    writer.writerow(headers)
-    print('Model answers count: ', len(model_answers))
-    for i in range(int(len(model_answers) / 10)):
-        write_format_flag = True
-        weighted_scores = []
-        for j in range(10):
-            model_answer = model_answers[i*10 + j].lower().replace('.', '').replace(',', '').replace('\'', '').replace('\"', '').replace('-', '').replace('%', '').replace(':', '')
-            correct_answer = correct_answers[i*10 + j].lower().replace('.', '').replace(',', '').replace('\'', '').replace('\"', '').replace('-', '').replace('%', '').replace(':', '')
-            gr_model_answer = gr_model_answers[i*10 + j].lower().replace('.', '').replace(',', '').replace('\'', '').replace('\"', '').replace('-', '').replace('%', '').replace(':', '')
-            gr_correct_answer = gr_correct_answers[i*10 + j].lower().replace('.', '').replace(',', '').replace('\'', '').replace('\"', '').replace('-', '').replace('%', '').replace(':', '')
+        if model_answer.isnumeric() and correct_answer.isnumeric():
+            for score_list in scores.values():
+                score_list.append('-')
+            total_score = 1 if model_answer == correct_answer else 0
+        else:
+            for score, score_list in scores.items():
+                cur_score = score_calculators[score](correct_answer, model_answer)
+                total_score += cur_score * weights[score]
+                score_list.append(cur_score)
+        total_scores.append(total_score)
+    
+    scores['total'] = total_scores
+    return scores
 
-            is_correct = is_correct_col[i*10 + j]
 
-            if model_answer.isnumeric() and correct_answer.isnumeric():
-                nlp_score = '-'
-                levenshtein_score = '-'
-                substring_score = '-'
-                sentence_transformers_score = '-'
-                gr_sentence_transformers_score = '-'
-                f1_score = '-'
-                weighted_score = 1 if model_answer == correct_answer else 0
-            else:
-                #Calculate scores:
-                levenshtein_distance = Levenshtein.distance(model_answer, correct_answer)
-                levenshtein_score = float("{:.3f}".format(1 - ( float(levenshtein_distance) / (max(len(model_answer), len(correct_answer))))))
+def write_headers(filename, headers):
+    with open('../output_data/' + filename, 'w', encoding='UTF16') as file:
+        writer = csv.writer(file)
+        writer.writerow(headers)
 
-                nlp_score = float("{:.3f}".format(nlp(model_answer).similarity(nlp(correct_answer))))
 
-                substring_score = float("{:.3f}".format(SequenceMatcher(None, model_answer, correct_answer).ratio()))
-
-                sentence_embeddings = sentence_transformer.encode([model_answer, correct_answer])
-                sentence_transformers_score = float("{:.3f}".format(util.pytorch_cos_sim(sentence_embeddings[0], sentence_embeddings[1])[0][0].item()))
-
-                sentence_embeddings = sentence_transformer_multilingual.encode([gr_model_answer, gr_correct_answer])
-                gr_sentence_transformers_score = float("{:.3f}".format(util.pytorch_cos_sim(sentence_embeddings[0], sentence_embeddings[1])[0][0].item()))
-                
-                print("model_answer: ", model_answer)
-                print("correct_answer: ", correct_answer)
-                common_words_count = count_common_words(model_answer, correct_answer)
-                precision = common_words_count / len(model_answer.split(" "))
-                recall = common_words_count / len(correct_answer.split(" "))
-                f1_score = 0 if precision == 0 and recall == 0 else (2 * precision * recall) / (precision + recall)
-
-                weighted_score = nlp_score * 0.2 + levenshtein_score * 0.1 + substring_score * 0.1 + sentence_transformers_score * 0.4 + f1_score * 0.2
-                weighted_scores.append(weighted_score)
-
-            # Write to csv
-            if write_format_flag is True:
-                writer.writerow([questions[i*10].replace(',', ''), models[j], model_answers[i*10 + j], correct_answers[i*10 + j], model_answer, correct_answer, gr_model_answers[i*10 + j], gr_correct_answers[i*10 + j], model_prediction_scores[i*10 + j], nlp_score, levenshtein_score, substring_score, sentence_transformers_score, gr_sentence_transformers_score, f1_score, weighted_score, is_correct])
-                write_format_flag = False    
-            else:
-                writer.writerow(['', models[j], model_answers[i*10 + j], correct_answers[i*10 + j], model_answer, correct_answer, gr_model_answers[i*10 + j], gr_correct_answers[i*10 + j], model_prediction_scores[i*10 + j], nlp_score, levenshtein_score, substring_score, sentence_transformers_score, gr_sentence_transformers_score, f1_score, weighted_score, is_correct])
+def write_scores(data, scores, filename):
+    headers = ['question', 'model', 'gr_correct_answer', 'gr_model_answer', 'en_correct_answer', 
+               'en_model_answer', 'confidence_score', 'nlp_score', 'levenshtein_score', 
+               'substring_score', 'sentence_transformer_score', 'f1_score', 'total_score']
+    write_headers(filename, headers)
+               
+    with open('../output_data/' + filename, 'a', encoding='UTF16') as file:
+        writer = csv.writer(file)
+        for i in range(len(scores['total'])):
+            writer.writerow([
+                data['question'][i], 
+                data['model'][i % len(data['model'])],
+                data['gr_correct_answer'],
+                data['gr_model_answer'],
+                data['en_correct_answer'],
+                data['en_model_answer'],
+                data['confidence_score'],
+                scores['nlp'][i],
+                scores['levenshtein'][i],
+                scores['substring'][i],
+                scores['sentence_transformer'][i],
+                scores['f1'][i],
+                scores['total'][i]
+            ])
 
 
 if __name__ == "__main__":
-    pass
-    #refactoring needed
+    input_dir = '../../1.BERT_models_QnA/output_data/'
+    input_filename = select_input_file(input_dir)
+
+    data = get_QnA_results(input_dir + input_filename)
+    scores = calculate_scores(data['en_correct_answer'], data['en_model_answer'], )
+
+    output_filename = get_output_filename(input_filename)
+    write_scores(data, scores, output_filename)
