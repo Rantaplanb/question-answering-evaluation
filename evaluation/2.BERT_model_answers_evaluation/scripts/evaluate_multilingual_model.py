05517252446b8120304e2ba8cc5e0f8b41aee77c
rantaplanb <csd4457@csd.uoc.gr>
2022-06-18 20:03:41 +0300

    Added f1 score in the multilingual model.

--- evaluation/2.BERT_model_answers_evaluation/scripts/evaluate_multilingual_model.py
+++ evaluation/2.BERT_model_answers_evaluation/scripts/evaluate_multilingual_model.py
@@ -40,23 +40,45 @@ def calc_multilingual_sentence_transformer_score(correct_answer, model_answer):
     sentence_embeddings = sentence_transformer_multilingual.encode([model_answer, correct_answer])
     return float("{:.3f}".format(util.pytorch_cos_sim(sentence_embeddings[0], sentence_embeddings[1])[0][0].item()))
 
+def count_common_words(s0, s1):
+    s0 = s0.lower()
+    s1 = s1.lower()
+    s0List = s0.split(" ")
+    s1List = s1.split(" ")
+    return len(list(set(s0List)&set(s1List)))
 
-def calculate_scores(correct_answers, model_answers):
+def calc_f1_score(correct_answer, model_answer):
+    common_words_count = count_common_words(model_answer, correct_answer)
+    precision = common_words_count / len(model_answer.split(" "))
+    recall = common_words_count / len(correct_answer.split(" "))
+    return 0 if precision == 0 and recall == 0 else float("{:.3f}".format((2 * precision * recall) / (precision + recall)))
+
+def calculate_transformer_scores(correct_answers, model_answers):
     multilingual_sentence_transformer_scores = []
     for i in range(len(correct_answers)):
-        total_score = 0
         correct_answer = format_answer(correct_answers[i])
         model_answer = format_answer(model_answers[i])
 
         if model_answer.isnumeric() and correct_answer.isnumeric():
-            for score_list in scores.values():
-                score_list.append('-')
-            total_score = 1 if model_answer == correct_answer else 0
+            multilingual_sentence_transformer_scores.append(1 if model_answer == correct_answer else 0)
         else:
             multilingual_sentence_transformer_scores\
                 .append(calc_multilingual_sentence_transformer_score(correct_answer, model_answer))
-    
     return multilingual_sentence_transformer_scores
+
+
+def calculate_f1_scores(correct_answers, model_answers):
+    f1_scores = []
+    for i in range(len(correct_answers)):
+        correct_answer = format_answer(correct_answers[i])
+        model_answer = format_answer(model_answers[i])
+
+        if model_answer.isnumeric() and correct_answer.isnumeric():
+            f1_scores.append(1 if model_answer == correct_answer else 0)
+        else:
+            f1_scores\
+                .append(calc_f1_score(correct_answer, model_answer))
+    return f1_scores
 
 
 def get_output_filename(input_filename):
@@ -74,20 +96,21 @@ def write_headers(filename, headers):
         writer = csv.writer(file)
         writer.writerow(headers)
 
-def write_scores(data, scores, filename):
+def write_scores(data, trans_scores, f1_scores, filename):
     headers = ['question', 'correct_answer', 'model_answer', 
-            'confidence_score', 'sentence_transformer_score']
+            'confidence_score', 'sentence_transformer_score', 'f1_score']
     write_headers(filename, headers)
                
     with open('../output_data/' + filename, 'a', encoding='UTF16') as file:
         writer = csv.writer(file)
-        for i in range(len(scores)):
+        for i in range(len(trans_scores)):
             writer.writerow([
                 '' if str(data['questions'][i]) == 'nan' else data['questions'][i], 
                 data['correct_answers'][i],
                 data['model_answers'][i],
                 data['confidence_scores'][i],
-                scores[i],
+                trans_scores[i],
+                f1_scores[i],
             ])
 
 
@@ -96,7 +119,8 @@ if __name__ == "__main__":
     input_filename = select_input_file(input_dir)
 
     data = get_QnA_results(input_dir + input_filename)
-    scores = calculate_scores(data['correct_answers'], data['model_answers'])
+    trans_scores = calculate_transformer_scores(data['correct_answers'], data['model_answers'])
+    f1_scores = calculate_f1_scores(data['correct_answers'], data['model_answers'])
 
     output_filename = get_output_filename(input_filename)
-    write_scores(data, scores, output_filename)
+    write_scores(data, trans_scores, f1_scores, output_filename)
