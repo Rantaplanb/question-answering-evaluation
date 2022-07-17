a24103ab314caf0d678f31ad7e117e4ddf1f3e06
mertzanis_l1 <loukas.mertzanis@softcom-int.com>
2022-07-18 02:19:14 +0300

    qna on xsquad with bing

--- evaluation/utils/text_splitter.py
+++ evaluation/utils/text_splitter.py
@@ -9,11 +9,25 @@ documenter = DocumentAssembler().setInputCol("text").setOutputCol("document")
 sentencerDL = SentenceDetectorDLModel.pretrained("sentence_detector_dl", "xx").setInputCols(["document"]).setOutputCol("sentences")
 sd_model = LightPipeline(PipelineModel(stages=[documenter, sentencerDL]))
 
+def findnth(string, substring, n):
+    parts = string.split(substring, n + 1)
+    if len(parts) <= n + 1:
+        return -1
+    return len(string) - len(parts[-1]) - len(substring)
+
 def split_to_sentences(text):
     sentences=[]
     for anno in sd_model.fullAnnotate(text)[0]["sentences"]:
         sentences.append(anno.result)
-    return sentences
+    res = []
+    for sent in sentences:
+        if len(sent) > 999:
+            str_index = findnth(sent, ' ', int(sent.count(' ') / 2))
+            res.append(sent[0:str_index])
+            res.append(sent[str_index:])
+        else:
+            res.append(sent)
+    return res
 
 
 def split_text(text, length_limit):
